1b62c52f32013661c0d30eb6a8d5c50b3ea3fd72
kyannavaja <navakl009@gmail.com>
2026-05-18 17:56:16 +0100

    Update main.js
    skeleton code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+/*
+  Main App Logic
+  ----------------
+  Handles input -> graph -> Socratic question.
+*/
 
+const input = document.getElementById("user-input");
+
+input.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    const userText = input.value.trim();
+    if (!userText) return;
+
+    // Display user message
+    UI.addMessage("user", userText);
+
+    // Add user node
+    const userNodeId = ReasoningGRaph.addNode(userText, "claim");
+
+    //Generate Socratic question
+    const botQuestion = SocraticEngine.generateQuestion(userText);
+
+    // Display bot message
+    UI.addMessage("bot", botQuestion);
+
+    // Add bot node
+    const botNodeId = ReasoningGraph.addNode(botQuestion, "question");
+
+    // Link nodes
+    ReasoningGraph.addEdge(boteNodeId, userNodeId, "challenges");
+
+    // Update graph panel
+    UI.updateGRaph();
+
+    input.value = "";
+  }
+});
