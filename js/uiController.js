f7468d2df22727bc6a2dfd266293a30fb96a46ac
kyannavaja <navakl009@gmail.com>
2026-05-18 17:50:20 +0100

    Update uiController.js
    skeleton code

--- js/uiController.js
+++ js/uiController.js
@@ -1,1 +1,23 @@
+/*
+  UI Controller
+  ----------------
+  Hanles chat + graph updates.
+*/
 
+const UI = {
+
+  chatlog: document.getElementById("chat-log"),
+  graphOutput: document.getElementById("graph-output"),
+
+  addMessage(sender, text) {
+    const div = document.createElement("div");
+    div.className = sender;
+    div.textContent = text;
+    this.chatLog.appendChild(div);
+    this.chatLog.scrollTop = this.chatLog.scrollHeight;
+  },
+
+  updateGraph() {
+    this.graphOutput.textContent = ReasoningGraph.render();
+  }
+};
