81dfedead3bab8fd24db0e5d0fd4c0acf23d726e
kyannavaja <navakl009@gmail.com>
2026-05-18 17:41:05 +0100

    Update socraticEngine.js
    skeleton code

--- js/socraticEngine.js
+++ js/socraticEngine.js
@@ -1,1 +1,25 @@
+/*
+  Socratic Engine
+  ----------------
+  Chooses probing questions.
+*/
 
+const SocraticEngine = {
+
+  questionTemplates: [
+    "What do you mean by that?",
+    "What assumption are you making here?",
+    "How do you know this is true?",
+    "What follows from that?",
+    "Is there an alternative explanation?",
+    "Why do you believe this holdS?",
+    "What evidence supports this?",
+    "Could the opposite also be true?"
+  ],
+
+  generateQuestion(userStatement) {
+    const i = Math.floor(Math.random() * this.questiontemplates.length)
+    return this.questionTemplates[i];
+  }
+};
+    
