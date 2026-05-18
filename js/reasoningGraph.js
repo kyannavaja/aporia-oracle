4279c5d0f90837c0aa44ea9530c780cfcc842a72
kyannavaja <navakl009@gmail.com>
2026-05-18 17:46:38 +0100

    Update reasoningGraph.js
    skeleton code

--- js/reasoningGraph.js
+++ js/reasoningGraph.js
@@ -1,1 +1,29 @@
+/*
+  Reasoning Graph (Golden Thread)
+  --------------------------------
+  Stores nodes + edges.
+*/
 
+const ReasoningGraph = {
+
+  nodes: [],
+  edges: [],
+
+  addNode(text, type = "claim") {
+    const id = this.nodes.length + 1;
+    this.nodes.push({id, text, type});
+    return id;
+  },
+
+  addEdge(fromId, toId, type = "supports") {
+    this.edges.push({fromId, toId, type});
+  },
+
+  render() {
+    let out = "";
+    this.nodes.forEach(n=> {
+      out += `[${n.id}] (${n.type}) ${n.text}\n`;
+    });
+    return out;
+  }
+};
