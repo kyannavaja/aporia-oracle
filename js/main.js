d98929a6c15de7b1c413a820b766d8ac7be1c859
Kyan Navaja <navakl009@gmail.com>
2026-05-24 16:19:08 +0100

    Update main.js
    line 21, bot question appears with a "question" type marker [attempt]

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ input.addEventListener("keydown", function(event) {
     const userNodeId = ReasoningGraph.addNode(userText, "claim");
 
     //Generate Socratic question
-    const botQuestion = SocraticEngine.generateQuestion(userText);
+    const botQuestion = SocraticEngine.generateQuestion(userText, "question");
 
     // Display bot message
     UI.addMessage("bot", botQuestion);
